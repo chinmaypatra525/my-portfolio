2e0b9ff0dfcf299f7add12cf154f02e0206314ee
chinmaypatra525 <chinmaypatra525@gmail.com>
2025-01-08 23:09:56 +0530

    first commit

---
+++ app/components/Footer.jsx
@@ -0,0 +1,48 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Footer = ({ isDarkMode }) => {
+  return (
+    <div>
+      <div>
+        <Image
+          src={isDarkMode ? assets.logo_dark : assets.logo}
+          alt=""
+          className="w-48 mx-auto mb-2"
+        />
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image
+            src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
+            alt=""
+            className="w-6"
+          />
+          chinmaypatra525@gmail.com
+        </div>
+      </div>
+
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6 ">
+        <p>© 2025 Chinmay Kumar Patra. All right reserved.</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/chinmaypatra525">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/chinmaypatra525">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/chinmaypatra525">
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
