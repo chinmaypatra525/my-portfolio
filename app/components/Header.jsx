ce5caeedf85fb678f84a260c8a71890f9029ed12
Chinmay Kumar Patra <89455544+chinmaypatra525@users.noreply.github.com>
2026-03-14 23:29:43 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
         transition={{ duration: 0.8, delay: 0.5 }}
         className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo"
       >
-        Gen AI AI Engineer based out of Bangalore.
+        Gen AI Engineer based out of Bangalore.
       </motion.h1>
       <motion.p
         initial={{ opacity: 0 }}
